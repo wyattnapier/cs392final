ae5ce78c0c269adb29524703e2863c9c0849b419
Matthew Berk <mberk@bu.edu>
2023-12-07 09:52:10 -0500

    moved files into better folders. added header back

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
 import React from 'react';
-import DogPersonPage from './components/home/DogPersonPage.js';
-import CatPersonPage from './components/home/CatPerson.js'
+import DogPersonPage from './components/result-pages/DogPersonPage.js';
+import CatPersonPage from './components/result-pages/CatPerson.js'
 import Header from './components/header/Header.js';
 import QuestionPage from "./components/questions/QuestionPage"
 import HomePage from "./components/home/Home";
@@ -11,6 +11,7 @@ import HomePage from "./components/home/Home";
 function App() {
   return (
     <div className="App">
+        <Header/>
       <HomePage/>
     </div>
   );
